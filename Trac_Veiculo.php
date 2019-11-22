a4fe3104d400db1c5118c76afc1289b232f92a9e
Thiago Guarniari <thiagoguarniari@gmail.com>
2019-11-22 10:29:25 -0300

    Correção veículo

--- Trac_Veiculo.php
+++ Trac_Veiculo.php
@@ -398,16 +398,6 @@
                       ,"validar"        : ["notnull"]                      
                       ,"ajudaCampo"     : ["Nome da unidade."]
                       ,"padrao":0}
-            ,{"id":2  ,"field"          : "GPO_NOME"   
-                      ,"labelCol"       : "GRUPO OPERACIONAL"
-                      ,"obj"            : "edtGrpOpr"
-                      ,"tamGrd"         : "25em"
-                      ,"tamImp"         : "80"
-                      ,"digitosMinMax"  : [3,40]
-                      ,"digitosValidos" : "A|B|C|D|E|F|G|H|I|J|K|L|M|N|O|P|Q|R|S|T|U|V|W|X|Y|Z|0|1|2|3|4|5|6|7|8|9| "
-                      ,"ajudaCampo"     : ["Nome do veiculo com até 20 caracteres."]
-                      ,"importaExcel"   : "S"                                          
-                      ,"padrao":0}
             ,{"id":9  ,"field"          : "VCL_ATIVO"  
                       ,"labelCol"       : "ATIVO"   
                       ,"obj"            : "cbAtivo"
